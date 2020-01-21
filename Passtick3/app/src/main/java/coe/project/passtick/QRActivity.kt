891a7fec0cd21b2773e0495052e73f458d08b9db
Theerasan Suanse <theerasan@kkumail.com>
2020-01-21 21:42:43 +0700

    bottom navigation

---
+++ Passtick3/app/src/main/java/coe/project/passtick/QRActivity.kt
@@ -0,0 +1,12 @@
+package coe.project.passtick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QRActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr)
+    }
+}
